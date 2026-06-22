d7675deac4f7e8ea0b11f6f13bf6c4127a306b9a
Marco Groot <mg.marcogroot@gmail.com>
2026-06-23 08:36:50 +1000

    -

--- Day02.java
+++ Day02.java
@@ -1,19 +1,63 @@
 import java.util.*;
 import java.nio.file.*;
 
+
 public class Day02 {
-    public static Integer part1(List<String> input) {
-        return -1;
+    record PasswordSchema(Integer fewest, Integer most, Character letter, String password) {
+
+        Boolean isValidP1() {
+            var charOccurences = password.chars().filter(c -> c == letter).count();
+            return charOccurences >= fewest() && charOccurences <= most;
+        }
+
+        Boolean isValidP2() {
+            var first = false;
+            if (fewest() - 1 >= 0) {
+                first = password.charAt(fewest() - 1) == letter();
+            }
+
+            var second = false;
+            if (most() - 1 >= 0) {
+                second = password.charAt(most() - 1) == letter();
+            }
+
+            return first ^ second;
+        }
+
+
     }
 
-    public static Integer part2(List<String> input) {
-        return -1;
+    private static List<PasswordSchema> parseInput(List<String> input) {
+        var passwordSchemas = input.stream().map(it -> {
+            var pieces = it.split(" ");
+            var numRangeStrings = pieces[0].split("-");
+            Integer start = Integer.valueOf(numRangeStrings[0]);
+            Integer end = Integer.valueOf(numRangeStrings[1]);
+
+            var letter = pieces[1].charAt(0);
+            var password = pieces[2];
+
+            return new PasswordSchema(start, end, letter, password);
+        }).toList();
+
+        return passwordSchemas;
+    }
+
+    public static Long part1(List<String> input) {
+        var passwordSchemas = parseInput(input);
+        return passwordSchemas.stream().map(password -> password.isValidP1()).filter(b -> b)
+                .count();
+    }
+
+    public static Long part2(List<String> input) {
+        var passwordSchemas = parseInput(input);
+        return passwordSchemas.stream().map(password -> password.isValidP2()).filter(b -> b)
+                .count();
     }
 
     public static void main(String[] args) throws Exception {
-        var input = Files.readAllLines(Path.of("day2.txt")).stream()
-            .filter(s -> !s.isBlank())
-            .toList();
+        var input =
+                Files.readAllLines(Path.of("day2.txt")).stream().filter(s -> !s.isBlank()).toList();
         System.out.println(part1(input));
         System.out.println(part2(input));
     }
--- Day03.java
+++ Day03.java
@@ -2,18 +2,63 @@ import java.util.*;
 import java.nio.file.*;
 
 public class Day03 {
-    public static Integer part1(List<String> input) {
-        return -1;
+    record Slope(Integer row_slope, Integer col_slope) {
+    };
+
+    private static Long dfs(List<String> input, Integer row, Integer col) {
+        Integer new_col = col;
+        if (row >= input.size())
+            return 0L;
+        if (new_col >= input.get(0).length())
+            new_col = new_col % input.get(0).length();
+
+        var curr = input.get(row).charAt(new_col);
+        var treeCount = (curr == '#') ? 1L : 0L;
+        return dfs(input, row + 1, new_col + 3) + treeCount;
     }
 
-    public static Integer part2(List<String> input) {
-        return -1;
+    private static Long dfsTwo(List<String> input, Integer row, Integer col, Integer rowIncrease,
+            Integer columnIncrease) {
+        Integer new_col = col;
+        if (row >= input.size())
+            return 0L;
+        if (new_col >= input.get(0).length())
+            new_col = new_col % input.get(0).length();
+
+        var curr = input.get(row).charAt(new_col);
+        var treeCount = (curr == '#') ? 1L : 0L;
+        return dfsTwo(input, row + rowIncrease, new_col + columnIncrease, rowIncrease,
+                columnIncrease) + treeCount;
+    }
+
+    public static Long part1(List<String> input) {
+        return dfs(input, 0, 0);
+    }
+
+    public static Long part2(List<String> input) {
+
+
+        var slopes = List.of(new Slope(1, 1), new Slope(1, 3), new Slope(1, 5), new Slope(1, 7),
+                new Slope(2, 1));
+
+        var treeList = slopes.stream()
+                .map(slope -> dfsTwo(input, 0, 0, slope.row_slope(), slope.col_slope())).toList();
+
+
+        var answer = 1L;
+
+        for (var treeCount : treeList) {
+            System.out.println("Tree count: " + treeCount);
+            answer *= treeCount;
+        }
+
+        return answer;
+
     }
 
     public static void main(String[] args) throws Exception {
-        var input = Files.readAllLines(Path.of("day3.txt")).stream()
-            .filter(s -> !s.isBlank())
-            .toList();
+        var input =
+                Files.readAllLines(Path.of("day3.txt")).stream().filter(s -> !s.isBlank()).toList();
         System.out.println(part1(input));
         System.out.println(part2(input));
     }
