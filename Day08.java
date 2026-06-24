9b8c4483cd1e138f296d2b2ec61ba744e33aeaae
Marco Groot <mg.marcogroot@gmail.com>
2026-06-24 16:56:16 +1000

    -

--- Day08.java
+++ Day08.java
@@ -2,18 +2,81 @@ import java.util.*;
 import java.nio.file.*;
 
 public class Day08 {
+    record Instruction(String kind, Integer amount) {
+
+    }
+
+    private static ArrayList<Instruction> parseInput(List<String> input) {
+        return new ArrayList<>(input.stream().map(line -> {
+            var parts = line.split(" ");
+            var instruction = parts[0];
+            var sign = parts[1].charAt(0);
+            var magnitude = Integer.valueOf(parts[1].substring(1));
+            if (sign == '-') {
+                magnitude *= -1;
+            }
+
+            return new Instruction(instruction, magnitude);
+        }).toList());
+    }
+
+    private static Long solve(
+
+            List<Instruction> instructions) {
+
+        var seen = new HashSet<Integer>();
+        var i = 0;
+
+        Long sum = 0L;
+        while (true) {
+            if (i == instructions.size()) {
+                System.out.println(sum);
+                return -1L;
+            }
+            if (seen.contains(i))
+                return sum;
+            seen.add(i);
+            var instruction = instructions.get(i);
+            var kind = instruction.kind();
+
+            if (kind.equals("nop")) {
+                i++;
+            } else if (kind.equals("acc")) {
+                i++;
+                sum += instruction.amount();
+            } else {
+                i += instruction.amount();
+            }
+        }
+    }
+
     public static Long part1(List<String> input) {
-        return -1L;
+        var instructions = parseInput(input);
+        return solve(instructions);
     }
 
     public static Long part2(List<String> input) {
+        var instructions = parseInput(input);
+
+        for (int i = 0; i < instructions.size(); i++) {
+            var curr = instructions.get(i);
+            if (curr.kind().equals("nop") || curr.kind().equals("jmp")) {
+                var instruct = curr.kind().equals("nop") ? "jmp" : "nop";
+                var newInstruction = new Instruction(instruct, curr.amount());
+                instructions.set(i, newInstruction);
+                solve(instructions);
+                instructions.set(i, curr);
+            }
+        }
+
+
         return -1L;
+
     }
 
     public static void main(String[] args) throws Exception {
-        var input = Files.readAllLines(Path.of("day8.txt")).stream()
-            .filter(s -> !s.isBlank())
-            .toList();
+        var input =
+                Files.readAllLines(Path.of("day8.txt")).stream().filter(s -> !s.isBlank()).toList();
         System.out.println(part1(input));
         System.out.println(part2(input));
     }
--- Day09.java
+++ Day09.java
@@ -3,17 +3,76 @@ import java.nio.file.*;
 
 public class Day09 {
     public static Long part1(List<String> input) {
+        // var startLength = 25;
+        // Queue<Long> previous = new LinkedList<>();
+        // for (int i = 0; i < startLength; i++) {
+        // previous.add(Long.valueOf(input.get(i)));
+        // }
+
+        // var nums = input.stream().map(line -> Long.valueOf(line)).toList();
+        // var subList = new ArrayList<>(nums.subList(startLength, nums.size()));
+
+        // for (Long num : subList) {
+        // // for (var a : previous) {
+        // // System.out.print(a + " ");
+        // // }
+        // // System.out.print("\n");
+        // var can = false;
+        // for (var a : previous) {
+        // for (var b : previous) {
+        // if (a != b) {
+        // Long c = a + b;
+        // if (c.equals(num)) {
+        // can = true;
+        // break;
+        // }
+        // }
+        // }
+        // }
+        // previous.poll();
+        // previous.add(num);
+        // if (!can) {
+
+
+        // return (long) num;
+        // }
+        // }
+
         return -1L;
     }
 
     public static Long part2(List<String> input) {
+        var nums = input.stream().map(line -> Long.valueOf(line)).toList();
+        Long goal = 15690279L;
+
+        for (int i = 0; i < nums.size(); i++) {
+            Long sum = 0L;
+            Long min = Long.MIN_VALUE;
+            Long max = Long.MAX_VALUE;
+
+            int j = i;
+            while (sum < goal) {
+                var curr = nums.get(j);
+                sum += curr;
+                min = Math.max(min, curr);
+                max = Math.min(max, curr);
+                if (sum.equals(goal)) {
+                    return max + min;
+                }
+                j++;
+            }
+            if (sum == goal) {
+                return max + min;
+            }
+        }
+
+
         return -1L;
     }
 
     public static void main(String[] args) throws Exception {
-        var input = Files.readAllLines(Path.of("day9.txt")).stream()
-            .filter(s -> !s.isBlank())
-            .toList();
+        var input =
+                Files.readAllLines(Path.of("day9.txt")).stream().filter(s -> !s.isBlank()).toList();
         System.out.println(part1(input));
         System.out.println(part2(input));
     }
